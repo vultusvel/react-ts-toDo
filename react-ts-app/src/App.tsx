ee094bbbecb0476bf134238ddb0244af3a064d0f
Anahit Gevorgyan <angevorgyan@griddynamics.com>
2023-10-15 18:06:48 +0400

    React TS TodoList app

---
+++ react-ts-app/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NewTodo from './NewToDo';
+import TodoList from './ToDoList';
+
+export interface Todo {
+  id: string;
+  text: string;
+}
+
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos(prevTodos => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text }
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
+
+export default App;
